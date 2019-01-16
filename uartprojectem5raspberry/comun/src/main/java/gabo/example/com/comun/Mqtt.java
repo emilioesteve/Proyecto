967cf0e88ccea77eca312eee8cd538ef80285552
EMILIO <emilioesteve99@icloud.com>
2019-01-16 18:34:16 +0100

    App + things
    Hecho por Gabi

--- uartprojectem5raspberry/comun/src/main/java/gabo/example/com/comun/Mqtt.java
+++
@@ -1,9 +0,0 @@
-package gabo.example.com.comun;
-
-public class Mqtt {
-    public static final String TAG = "MQTT";
-    public static final String topicRoot="grupo6/proyecto/";//Reemplaza jtomas
-    public static final int qos = 1;
-    public static final String broker = "tcp://iot.eclipse.org:1883";
-    public static final String clientId = "Raspberry";
-}

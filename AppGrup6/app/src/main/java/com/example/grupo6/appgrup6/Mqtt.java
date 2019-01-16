d52a4e3a3c0c360bfde8f305e182edb43a602f2c
EMILIO <emilioesteve99@icloud.com>
2019-01-16 18:01:11 +0100

    Cambios varios
    Arreglada la ruta de MQTT y el layout de
    sensores activity

--- AppGrup6/app/src/main/java/com/example/grupo6/appgrup6/Mqtt.java
+++ AppGrup6/app/src/main/java/com/example/grupo6/appgrup6/Mqtt.java
@@ -2,7 +2,7 @@ package com.example.grupo6.appgrup6;
 
 public class Mqtt {
     public static final String TAG = "MQTT";
-    public static final String topicRoot="grupo6/practica/";//Reemplaza jtomas
+    public static final String topicRoot="grupo6/proyecto/";//Reemplaza jtomas
     public static final int qos = 1;
     public static final String broker = "tcp://iot.eclipse.org:1883";
     public static final String clientId = "Test1345698986";

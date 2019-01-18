cc7a1f4f71623c51a4d96e89bc9b86de2ba5cd5f
Gabo <gasaga95@hotmail.com>
2019-01-18 14:29:47 +0100

    Proyecto- raspberry
    proyecto completo final de la raspberry

---
+++ uartprojectem5raspberry/comun/src/main/java/gabo/example/com/comun/Mqtt.java
@@ -0,0 +1,9 @@
+package gabo.example.com.comun;
+
+public class Mqtt {
+    public static final String TAG = "MQTT";
+    public static final String topicRoot="grupo6/proyecto/";//Reemplaza jtomas
+    public static final int qos = 1;
+    public static final String broker = "tcp://iot.eclipse.org:1883";
+    public static final String clientId = "Raspberry";
+}
